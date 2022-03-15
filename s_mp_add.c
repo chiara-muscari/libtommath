6d2c20d643618ba92354b9019d29152eef5cc5aa
Chiara <chiara.muscari@mail.polimi.it>
2022-03-15 23:44:11 +0100

    refactoring of basic operations (add/sub) code

--- s_mp_add.c
+++ s_mp_add.c
@@ -7,8 +7,16 @@
 mp_err s_mp_add(const mp_int *a, const mp_int *b, mp_int *c)
 {
    int oldused, min, max, i;
+#ifdef MP_32BIT
+   mp_digit tmp, tmp0;
+#endif
    mp_digit u;
    mp_err err;
+
+#ifdef STM32
+	volatile uint32_t max_num_digits;
+	volatile uint32_t base_address_op1, base_address_op2, base_address_res, limit_1, limit_2;
+#endif
 
    /* find sizes, we let |a| <= |b| which means we have to sort
     * them.  "x" will point to the input with the most digits
@@ -29,9 +37,82 @@ mp_err s_mp_add(const mp_int *a, const mp_int *b, mp_int *c)
    oldused = c->used;
    c->used = max + 1;
 
+#ifdef STM32
+
+	base_address_op1 = &(a->dp[0]);
+	base_address_op2 = &(b->dp[0]);
+	max_num_digits = a->used;
+	limit_1 = &(a->dp[b->used-1]);
+	limit_2 = &(a->dp[a->used-1]);
+	base_address_res = &(c->dp[0]);
+
+	__asm__ volatile (
+					"MOV %%r3, %0;"
+					"MOV %%r4, %1;"
+					"MOV %%r5, %2;"
+					"MOV %%r8, %3;"
+					"MOV %%r9, %4;"
+
+					"LDR %%r0, [%%r3];" // Load 1st operand
+					"LDR %%r1, [%%r4];" // Load 2nd operand
+					"ADDS %%r2, %%r0, %% r1 ;" // Perform the sum of the least significant digit
+					"STR %%r2, [%%r5];" // Store the result
+
+					"LOOP_1:" // Check for loop limitation
+					"SUB %%r2, %%r8, %%r3;" // TODO: change this into sub 1 and check for 0
+					"CBZ %%r2, LOOP_2;"
+
+					"ADD %%r3, %%r3, $4;" // Increase the address
+					"ADD %%r4, %%r4, $4;"
+					"ADD %%r5, %%r5, $4;"
+
+					"LDR %%r0, [%%r3];"
+					"LDR %%r1, [%%r4];"
+					"ADCS %%r2, %%r0, %%r1 ;" // Perform the sum, considering the carry from the previous one
+					"STR %%r2, [%%r5];"
+					"B LOOP_1;"
+
+					"LOOP_2:" // Now just the carry propagation has to be computed
+					"SUB %%r2, %%r9, %%r3;" // Check for end of loop
+					"CBZ %%r2, EXIT_2;"
+					"ADD %%r3, %%r3, $4;" // Increase the address
+					"ADD %%r5, %%r5, $4;"
+					"LDR %%r2, [%%r3];"
+					"ADCS %%r2, $0 ;"
+					"STR %%r2, [%%r5];"
+
+					"B LOOP_2;"
+					"EXIT_2:"
+					"ADD %%r5, %%r5, $4;"
+					"MOV %%r2, $0;"
+					"ADCS %%r2, $0;"
+					"STR %%r2, [%%r5];"
+
+
+					:
+					:"r" (base_address_op1), "r" (base_address_op2),
+						 "r" (base_address_res), "r" (limit_1), "r" (limit_2)
+					: "r0", "r1", "r2", "r3", "r4", "r5", "r8", "r9", "memory", "cc");
+#else
+
    /* zero the carry */
    u = 0;
    for (i = 0; i < min; i++) {
+#ifdef MP_32BIT
+		tmp0 = a->dp[i] + b->dp[i];
+		tmp = tmp0 + u;
+
+		u = 0;
+		if(tmp0 < a->dp[i]) {
+			u++;
+		}
+		if(tmp < tmp0) {
+			u++;
+		}
+		u = u%2;
+
+		c->dp[i] = tmp;
+#else
       /* Compute the sum at one digit, T[i] = A[i] + B[i] + U */
       c->dp[i] = a->dp[i] + b->dp[i] + u;
 
@@ -40,6 +121,7 @@ mp_err s_mp_add(const mp_int *a, const mp_int *b, mp_int *c)
 
       /* take away carry bit from T[i] */
       c->dp[i] &= MP_MASK;
+#endif
    }
 
    /* now copy higher words if any, that is in A+B
@@ -47,6 +129,16 @@ mp_err s_mp_add(const mp_int *a, const mp_int *b, mp_int *c)
     */
    if (min != max) {
       for (; i < max; i++) {
+#ifdef	MP_32BIT
+			tmp0 = a->dp[i] + u;
+			if(tmp0 < a->dp[i]) {
+				u = 1;
+			}
+			else {
+				u = 0;
+			}
+			c->dp[i] = tmp0;
+#else
          /* T[i] = A[i] + U */
          c->dp[i] = a->dp[i] + u;
 
@@ -55,11 +147,14 @@ mp_err s_mp_add(const mp_int *a, const mp_int *b, mp_int *c)
 
          /* take away carry bit from T[i] */
          c->dp[i] &= MP_MASK;
+#endif
       }
    }
 
    /* add carry */
    c->dp[i] = u;
+
+#endif
 
    /* clear digits above oldused */
    s_mp_zero_digs(c->dp + c->used, oldused - c->used);
--- s_mp_sub.c
+++ s_mp_sub.c
@@ -7,8 +7,16 @@
 mp_err s_mp_sub(const mp_int *a, const mp_int *b, mp_int *c)
 {
    int oldused = c->used, min = b->used, max = a->used, i;
+#ifdef MP_32BIT
+   mp_digit tmp, tmp0;
+#endif
    mp_digit u;
    mp_err err;
+
+#ifdef STM32
+	volatile uint32_t max_num_digits;
+	volatile uint32_t base_address_op1, base_address_op2, base_address_res, limit_1, limit_2;
+#endif
 
    /* init result */
    if ((err = mp_grow(c, max)) != MP_OKAY) {
@@ -17,9 +25,85 @@ mp_err s_mp_sub(const mp_int *a, const mp_int *b, mp_int *c)
 
    c->used = max;
 
+#ifdef STM32
+
+
+	base_address_op1 = &(a->dp[0]);
+	base_address_op2 = &(b->dp[0]);
+	max_num_digits = a->used;
+	limit_1 = &(a->dp[b->used-1]);
+	limit_2 = &(a->dp[a->used-1]);
+
+	base_address_res = &(c->dp[0]);
+
+
+	__asm__ volatile (
+					"MOV %%r3, %0;"
+					"MOV %%r4, %1;"
+					"MOV %%r5, %2;"
+					"MOV %%r8, %3;"
+					"MOV %%r9, %4;"
+
+					"LDR %%r0, [%%r3];"
+					"LDR %%r1, [%%r4];"
+					"SUBS %%r2, %%r0, %% r1 ;"
+					"STR %%r2, [%%r5];"
+
+					"LOOP_1:"
+					"SUB %%r2, %%r8, %%r3;"
+					"CBZ %%r2, LOOP_2;"
+
+					"ADD %%r3, %%r3, $4;"
+					"ADD %%r4, %%r4, $4;"
+					"ADD %%r5, %%r5, $4;"
+
+					"LDR %%r0, [%%r3];"
+					"LDR %%r1, [%%r4];"
+					"SBCS %%r2, %%r0, %%r1 ;"
+					"STR %%r2, [%%r5];"
+					"B LOOP_1;"
+
+					"LOOP_2:"
+					"SUB %%r2, %%r9, %%r3;"
+					"CBZ %%r2, EXIT_2;"
+					"ADD %%r3, %%r3, $4;"
+					"ADD %%r5, %%r5, $4;"
+					"LDR %%r2, [%%r3];"
+					"SBCS %%r2, $0 ;"
+					"STR %%r2, [%%r5];"
+
+					"B LOOP_2;"
+					"EXIT_2:"
+					"ADD %%r5, %%r5, $4;"
+					"MOV %%r2, $0;"
+					"SBCS %%r2, $0;"
+					"STR %%r2, [%%r5];"
+
+
+					:
+					:"r" (base_address_op1), "r" (base_address_op2),
+						 "r" (base_address_res), "r" (limit_1), "r" (limit_2)
+					: "r0", "r1", "r2", "r3", "r4", "r5", "r8", "r9", "memory", "cc");
+#else
+
    /* set carry to zero */
    u = 0;
    for (i = 0; i < min; i++) {
+#ifdef MP_32BIT
+		tmp0 = a->dp[i] - b->dp[i];
+		tmp = tmp0 - u;
+
+		u = 0;
+		if(tmp0 > a->dp[i]) {
+			u++;
+		}
+		if(tmp > tmp0) {
+			u++;
+		}
+		u = u%2;
+
+		c->dp[i] = tmp;
+#else
       /* T[i] = A[i] - B[i] - U */
       c->dp[i] = (a->dp[i] - b->dp[i]) - u;
 
@@ -32,10 +116,21 @@ mp_err s_mp_sub(const mp_int *a, const mp_int *b, mp_int *c)
 
       /* Clear carry from T[i] */
       c->dp[i] &= MP_MASK;
+#endif
    }
 
    /* now copy higher words if any, e.g. if A has more digits than B  */
    for (; i < max; i++) {
+#ifdef MP_32BIT
+		tmp0 = a->dp[i] - u;
+		if(tmp0 > a->dp[i]) {
+			u = 1;
+		}
+		else {
+			u = 0;
+		}
+		c->dp[i] = tmp0;
+#else
       /* T[i] = A[i] - U */
       c->dp[i] = a->dp[i] - u;
 
@@ -44,8 +139,11 @@ mp_err s_mp_sub(const mp_int *a, const mp_int *b, mp_int *c)
 
       /* Clear carry from T[i] */
       c->dp[i] &= MP_MASK;
+#endif
    }
 
+
+#endif
    /* clear digits above used (since we may not have grown result above) */
    s_mp_zero_digs(c->dp + c->used, oldused - c->used);
 
