7f60845062e46b8d92ddbcd10dd82902ef1a288c
Chiara <chiara.muscari@mail.polimi.it>
2022-05-07 19:23:48 +0200

    modified version of folded, now supports for any number of digits

--- mp_reduce_folded.c
+++ mp_reduce_folded.c
@@ -15,8 +15,29 @@ mp_err mp_reduce_folded(mp_int *x, const mp_int *m, const mp_int *mu, mp_int *pi
    mp_int  q; // x_p;
    mp_err  err;
    int     um = m->used;
+/*
+   if(m->dp[m->used-1] >> 26 == 0) { // less bits than m->used * MP_DIGIT_BIT
+	   if(m->used % 2 != 0) { // odd number
+		   um--; //consider like one digit less
+	   }
+	   else { // even number
+		   um-=2; // consider like 2 digits less, in order to preserve eveness
+	   }
+   }
+   else { // bits number = m->used * MP_DIGIT_BIT
+	   if(m->used % 2 != 0) { // odd number
+		   um++; //consider one digit more
+	   }
+	   // if even, leave as it is
+   }
+   */
+
+   //if(m->used % 2 != 0)
+	   //um++;
+
    int s = um/2;
    int s_3 = 3*s;
+
 
    /* q = x */
    if ((err = mp_init_copy(&q, x)) != MP_OKAY) {
@@ -39,27 +60,32 @@ mp_err mp_reduce_folded(mp_int *x, const mp_int *m, const mp_int *mu, mp_int *pi
 
    if ((err = mp_add(x, &q, x)) != MP_OKAY) {
 		 goto LBL_ERR;
-	 }
+   }
 
 
    // From now on, it's like the standard reduce func
 
    mp_copy(x, &q);
 
+
    /*q = floor(x_p / 2**um)*/
-   mp_rshd(&q, um);
+   mp_rshd(&q, um-1); // TEST, um original value
 
    /* q = q * mu */
 
    //if ((err = mp_mul(&q, mu, &q)) != MP_OKAY) {
-   if ((err = s_mp_mul_high(&q, mu, &q, s-1)) != MP_OKAY) {
+   if ((err = s_mp_mul_high(&q, mu, &q, s+1)) != MP_OKAY) {
 		 goto LBL_ERR;
-	 }
+   }
 
    //mp_mul_v32_highNdigs(&q, mu, &q, s-1);
 
    /* q = q / b**(k/2) */
-   mp_rshd(&q, s);
+   //mp_rshd(&q, s); TEST
+   mp_rshd(&q, s+2);
+
+
+   //mp_div(x, m, &q, NULL);
 
    /* q = q * m mod b**(k+2), quick (no division) */
    if ((err = s_mp_mul(&q, m, &q, um + 2)) != MP_OKAY) {
@@ -70,6 +96,7 @@ mp_err mp_reduce_folded(mp_int *x, const mp_int *m, const mp_int *mu, mp_int *pi
       goto LBL_ERR;
    }
 
+#ifdef MP_32BIT
    if(mp_cmp_mag(x, &q) == MP_LT) {
 	   mp_grow(x, um+3);
 	   x->dp[um+2] = 1;
@@ -81,6 +108,21 @@ mp_err mp_reduce_folded(mp_int *x, const mp_int *m, const mp_int *mu, mp_int *pi
       goto LBL_ERR;
    }
 
+#else
+   if ((err = mp_sub(x, &q, x)) != MP_OKAY) {
+      goto LBL_ERR;
+   }
+   /* If x < 0, add b**(k+1) to it */
+   if (mp_cmp_d(x, 0uL) == MP_LT) {
+      mp_set(&q, 1uL);
+      if ((err = mp_lshd(&q, um + 1)) != MP_OKAY) {
+         goto LBL_ERR;
+      }
+      if ((err = mp_add(x, &q, x)) != MP_OKAY) {
+         goto LBL_ERR;
+      }
+   }
+#endif
    /* Back off if it's too big */
    while (mp_cmp(x, m) != MP_LT) {
       if ((err = mp_sub(x, m, x)) != MP_OKAY) {
--- mp_reduce_folded_setup.c
+++ mp_reduce_folded_setup.c
@@ -9,9 +9,41 @@
 mp_err mp_reduce_folded_setup(mp_int *mu, mp_int *P_p, const mp_int *b)
 {
    mp_err err;
-   if ((err = mp_2expt(mu, (3 * b->used * MP_DIGIT_BIT) / 2)) != MP_OKAY) {
-      return err;
+   int n = b->used;
+
+   /*
+   if(b->used % 2 == 0) { // even
+	   if(b->dp[b->used-1] >> 26 == 0) {
+	   	   if ((err = mp_2expt(mu, (3 * (b->used-2) * MP_DIGIT_BIT) / 2)) != MP_OKAY) {
+	   	      return err;
+	   	   }
+	   }
+	   else if((err = mp_2expt(mu, (3 * b->used * MP_DIGIT_BIT) / 2)) != MP_OKAY) {
+	      return err;
+	   }
    }
-   return mp_div(mu, b, mu, P_p);
+   else {
+	   if(b->dp[b->used-1] >> 26 == 0) {
+	   	   if ((err = mp_2expt(mu, (3 * (b->used-1) * MP_DIGIT_BIT) / 2)) != MP_OKAY) {
+	   	      return err;
+	   	   }
+	   }
+	   else if((err = mp_2expt(mu, (3 * (b->used+1) * MP_DIGIT_BIT) / 2)) != MP_OKAY) {
+	      return err;
+	   }
+   }
+   */
+  // if(b->used % 2 != 0)
+	//   n++;
+
+   if ((err = mp_2expt(mu, (3 * n * MP_DIGIT_BIT) / 2)) != MP_OKAY) {
+	  return err;
+   }
+   mp_div(mu, b, NULL, P_p);
+
+   if ((err = mp_2expt(mu, MP_DIGIT_BIT*(3 * n / 2 + 1))) != MP_OKAY) {
+	  return err;
+   }
+   return mp_div(mu, b, mu, NULL);
 }
 #endif
